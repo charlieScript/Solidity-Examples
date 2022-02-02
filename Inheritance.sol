c240d38e57ae70dcb307e9474ec01fff5beb86ec
charlieScript <gozione@gmail.com>
2022-02-02 13:12:01 +0100

    inheriance

---
+++ Inheritance.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// parent
+contract A {
+    function foo() public pure virtual returns(string memory) {
+        return "A";
+    }
+
+    function bar() public pure virtual returns(string memory) {
+        return "A";
+    }  
+
+    function baz() public pure virtual returns(string memory) {
+        return "A";
+    }   
+}
+
+// child
+contract B is A {
+    function foo() public pure override returns(string memory) {
+        return "B";
+    }
+
+    function bar() public pure virtual override returns(string memory) {
+        return "AB";
+    } 
+
+}
+
+contract C is B {
+    function bar() public pure override returns(string memory) {
+        return "c";
+    }
+}
